d457b75f91ad9fd0a65cc989fac1757e62560681
SSuiren <90429452+SSuiren@users.noreply.github.com>
2022-09-25 00:49:59 -0700

    Add files via upload

---
+++ Assets/enemy_movement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*public class power_up : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShotBoost;
+    }
+
+    void ShotBoost()
+    {
+
+     
+
+    }
+}*/
